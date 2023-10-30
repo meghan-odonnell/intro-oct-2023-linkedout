78d99d18cafc160530822a476fdf2bda208d9650
Meghan O'Donnell <odonnell.meghan@gmail.com>
2023-10-30 16:57:14 -0400

    EOD Monday

--- frontend/frontend/src/app/pages/components/todo-entry.component.ts
+++ frontend/frontend/src/app/pages/components/todo-entry.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 import { CommonModule } from "@angular/common";
 
@@ -40,6 +40,7 @@ import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
   styles: [],
 })
 export class TodoEntryComponent {
+  @Output() itemAdded = new EventEmitter<string>();
   form = new FormGroup({
     description: new FormControl<string>(""),
   });
